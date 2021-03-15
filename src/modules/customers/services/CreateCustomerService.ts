ee328c1684e22ac7b0d21b1f8097b1f0cda968a2
Yan Oliveira <oliveira.yan02@gmail.com>
2021-03-15 18:41:32 -0300

    feat: ensure it's able to create a new customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,13 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    return this.customersRepository.create({ name, email });
   }
 }
 
